17db13a4bf1a71a51afb638013889a89111c7c68
erichilario <reacherichilario@gmail.com>
2019-03-09 00:20:01 +0930

    week 1 complete

--- week1/pizza.py
+++ week1/pizza.py
@@ -14,4 +14,19 @@ class Food():
 
 class Pizza(Food):
 	
+	def __init__(self,desc,price,*topping):
+		Food.__init__(self,desc,price)
+		self.toppings = topping
+
 	
+	def get_details(self):
+		return str('The %s will cost %.2f. Toppings are: %s' % (self.description, self.cost, self.toppings))
+		
+george = Pizza("12' Pizza, thin base", 15.50, "Ham", "Cheese", "Pineapple")
+luke = Pizza("5' Pizza thin", 9.95, "Cheese", "Tomato Base")
+joan = Food("Sandwich", 4.50)
+george = Pizza("18' Pizza, thin base", 15.50, "Ham", "Cheese", "Pineapple", "Tomato", "Beef", "Sausage", "Pepperoni", "Chilli Flakes")
+
+print(george.get_details())
+print(luke.get_details())
+print(joan.get_details())
